dffa662a22df44eb2c94e2c1779d6178ae15d501
ogushan-akin <ogzhnakn96@gmail.com>
2022-01-19 14:36:35 +0300

    dosya yuklemeleri tamamlandi

---
+++ MVVMproject/Model/News.swift
@@ -0,0 +1,15 @@
+//
+//  News.swift
+//  MVVMproject
+//
+//  Created by AKIN on 17.01.2022.
+//
+
+import Foundation
+
+struct News : Decodable {
+    
+    let title : String
+    let story : String
+    
+}
